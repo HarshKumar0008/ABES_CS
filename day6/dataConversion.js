0c8aed4564e61a4c57e97a7538e33c5ab934d151
Harsh Kumar <harshmishra9308690752@gmail.com>
2025-09-18 12:55:22 +0530

    secnd commit

---
+++ day6/dataConversion.js
@@ -0,0 +1,23 @@
+// let strToNum ="34" +34
+let strToNum ="34" -34
+console.log(strToNum);
+
+console.log(typeof(strToNum));
+
+let numToStr = 34 + "34"
+console.log(numToStr);
+console.log(typeof(numToStr));
+
+let a = "23" + 23 +(-67) +"67"+true+false;
+console.log(a);
+console.log(typeof(a));
+// let a = "23" + 23 +(-67) +"67"+true+false;
+// console.log(a);
+// console.log(typeof(a));
+console.log(typeof(NaN));
+console.log(typeof(null));
+console.log(typeof(NULL));
+
+
+
+
---
+++ day6/datatype.js
@@ -0,0 +1,10 @@
+let name ="harsh ";
+let age=21;
+let boolVal = false
+console.log(name);
+console.log(age);
+console.log(boolVal);
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(boolVal));
