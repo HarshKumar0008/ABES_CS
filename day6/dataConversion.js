8f4cb12be8bde2a562c8bd0033bc60f4d1bcbc1c
Harsh Kumar <harshmishra9308690752@gmail.com>
2025-09-18 13:02:11 +0530

    third commit

--- day6/dataConversion.js
+++ day6/dataConversion.js
@@ -19,5 +19,17 @@ console.log(typeof(null));
 console.log(typeof(NULL));
 
 
+console.log("explicit type conversion");
+console.log(Boolean("123"));
+console.log(Boolean());
+console.log(Boolean(null));
+console.log(Boolean(-1));
+console.log(Boolean(0));
+
+
+
+ 
+
+
 
 
